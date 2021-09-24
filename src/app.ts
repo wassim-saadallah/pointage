0031201f8b4916902c680ddbf0563d0c56eef843
Wassim Saadallah <wassim.saadallah@neoxam.com>
2021-09-24 10:58:12 +0200

    feat: add swagger documentation and cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,54 @@
 import { FastifyInstance } from 'fastify'
 import AutoLoad from 'fastify-autoload'
 import { join } from 'path'
+import fastifySwagger from 'fastify-swagger'
+import { EmployeeSchema } from './schemas/employee'
+import fastifyCors from 'fastify-cors'
 
 export default async function (fastify: FastifyInstance) {
+
+  // allow CORS
+  // for testing reasons we added full access
+  fastify.register(fastifyCors)
+
   // hello world route
   fastify.get('/', (_, reply) => {
     reply.send({ Hello: 'World' })
   })
+
+  // swagger api docuemntation
+  fastify.register(fastifySwagger, {
+    routePrefix: '/docs',
+    swagger: {
+      info: {
+        title: 'API Pointage',
+        description: 'Swagger documentation for Pointage API',
+        version: '0.0.1'
+      },
+      host: 'localhost:3000',
+      schemes: ['http'],
+      consumes: ['application/json'],
+      produces: ['application/json'],
+      tags: [
+        { name: 'employee', description: 'Employee related end-points' },
+        { name: 'checkin/checkout', description: 'Checkin/Checkout related end-points' }
+      ],
+      definitions: {
+        Employee: EmployeeSchema
+      }
+    },
+    uiConfig: {
+      docExpansion: 'full',
+      deepLinking: false
+    },
+    staticCSP: false,
+    exposeRoute: true
+  })
+
   // require all routes in `routes` folder
-  fastify.register(AutoLoad, { dir: join(__dirname, 'routes') })
+  fastify.register(AutoLoad, {
+    prefix: '/api',
+    dir: join(__dirname, 'routes'),
+    options: { prefix: '/api' }
+  })
 }
